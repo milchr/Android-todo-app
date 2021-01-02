87f331ec73fa628ea07f31d6c36c77552c266e5a
milchr <milchr96@gmail.com>
2021-01-02 18:13:40 +0100

    added items to MainActivity, toastMessage function and onclicklistener for switch activity

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.todoapp;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Data data;
+    private Button add, view;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add = findViewById(R.id.addButton);
+        view = findViewById(R.id.viewButton);
+        editText = findViewById(R.id.editTextNewTask);
+        data = new Data(this);
+    }
+
+    public void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/todoapp/taskActivity.java
+++ app/src/main/java/com/example/todoapp/taskActivity.java
@@ -1,27 +1,35 @@
 package com.example.todoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.database.Cursor;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class taskActivity extends AppCompatActivity {
 
     Data data;
     private ListView listView;
+    private Button addTaskActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
         data = new Data(this);
+        addTaskActivity = findViewById(R.id.buttonAddTaskActivity);
         listView = (ListView) findViewById(R.id.listViewTasks);
 
+
+        addTaskActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(taskActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
