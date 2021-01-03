0a40d53e68d03146522ebda179280b76ec29ee94
milchr <milchr96@gmail.com>
2021-01-03 16:50:21 +0100

    added EditTaskActivity and selecting task by id

--- app/src/main/java/com/example/todoapp/Data.java
+++ app/src/main/java/com/example/todoapp/Data.java
@@ -12,8 +12,8 @@ import android.util.Log;
 public class Data extends SQLiteOpenHelper {
     private SQLiteDatabase db;
 
-    public static final String TABLE_ROW_ID = "ID";
-    public static final String TABLE_ROW_TASK = "task";
+    public static final String COLUMN_ID = "ID";
+    public static final String COLUMN_TASK = "task";
     private static final String DB_NAME = "todo_db";
     private static final int DB_VERSION = 1;
     private static final String TABLE_NAME = "to_do_table";
@@ -25,7 +25,7 @@ public class Data extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + TABLE_ROW_TASK + " TEXT)";
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_TASK + " TEXT)";
         db.execSQL(createTable);
     }
 
@@ -38,7 +38,7 @@ public class Data extends SQLiteOpenHelper {
     public boolean insertData(String item) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(TABLE_ROW_TASK, item);
+        contentValues.put(COLUMN_TASK, item);
 
         Log.i("dodawanie", "dodawanie " + item + " do " + TABLE_NAME);
         long result = db.insert(TABLE_NAME, null, contentValues);
@@ -54,6 +54,14 @@ public class Data extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         String query = "SELECT * FROM " + TABLE_NAME;
         Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public Cursor getId(String item){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COLUMN_ID + " FROM " + TABLE_NAME + " WHERE " + COLUMN_TASK + " = '" + item + "'";
+        Cursor data = db.rawQuery(query, null);
+
         return data;
     }
 
